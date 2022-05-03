9ce15ba0f58464ab0c87ec9ac72ae94914bb430d
sfzza <nurulizza.safira@gmail.com>
2022-05-03 14:07:17 +0700

    fix widget popular laundry

--- lib/pages/widgets/popular_laundry.dart
+++ lib/pages/widgets/popular_laundry.dart
@@ -24,76 +24,80 @@ class _popularlaundryState extends State<popularlaundry> {
   // popularlaundry(
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {},
-      child: Container(
-        width: 250,
-        height: 260,
-        decoration: BoxDecoration(
-          color: Color(0xff6998AB),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.only(top: 9),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Center(
-                child: Container(
-                  width: 234,
-                  height: 146,
-                  child: FittedBox(
-                    alignment: Alignment.topCenter,
-                    fit: BoxFit.fill,
-                    child: Image.asset(
-                      'assets/fotolaundry.png',
+    return Padding(
+      padding: const EdgeInsets.only(right: 30),
+      child: InkWell(
+        onTap: () {},
+        child: Container(
+          width: 250,
+          height: 260,
+          decoration: BoxDecoration(
+            color: Color(0xff6998AB),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.only(top: 9),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: Container(
+                    width: 234,
+                    height: 146,
+                    child: FittedBox(
+                      alignment: Alignment.topCenter,
+                      fit: BoxFit.fill,
+                      child: Image.asset(
+                        'assets/fotolaundry.png',
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 6, left: 8),
-                child: Text(widget.laundry_name, style: titleLaundryTextStyle),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 12, left: 8),
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.location_pin,
-                      size: 15,
-                      color: Color(0xff1A374D),
-                    ),
-                    SizedBox(
-                      width: 3,
-                    ),
-                    Expanded(
-                      child: Text(
-                        widget.laundry_address,
-                        style: subtitleLaundryTextStyle,
-                        overflow: TextOverflow.ellipsis,
-                        maxLines: 1,
+                Padding(
+                  padding: const EdgeInsets.only(top: 6, left: 8),
+                  child:
+                      Text(widget.laundry_name, style: titleLaundryTextStyle),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 12, left: 8, right: 8),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.location_pin,
+                        size: 15,
+                        color: Color(0xff1A374D),
                       ),
-                    ),
-                  ],
+                      SizedBox(
+                        width: 3,
+                      ),
+                      Expanded(
+                        child: Text(
+                          widget.laundry_address,
+                          style: subtitleLaundryTextStyle,
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 1,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 12, left: 8),
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.access_time,
-                      size: 15,
-                      color: Color(0xff1A374D),
-                    ),
-                    SizedBox(
-                      width: 3,
-                    ),
-                    Text("null", style: subtitleLaundryTextStyle),
-                  ],
-                ),
-              )
-            ],
+                Padding(
+                  padding: const EdgeInsets.only(top: 12, left: 8),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.access_time,
+                        size: 15,
+                        color: Color(0xff1A374D),
+                      ),
+                      SizedBox(
+                        width: 3,
+                      ),
+                      Text("null", style: subtitleLaundryTextStyle),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
